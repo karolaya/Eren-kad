592b9446330b60a289197aaa1fa8b2b4c3a64970
Dayan <dayanm@uninorte.edu.co>
2017-06-15 11:50:32 -0500

    Imagenes de los productos

--- eren-kad/app/models/product.rb
+++ eren-kad/app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-
+	has_attached_file :image, styles: { medium: '200x200>', thumb: '48x48>' }
 	belongs_to :category
 
 end
--- eren-kad/db/schema.rb
+++ eren-kad/db/schema.rb
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 20170615024741) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
